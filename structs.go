7874ea1a80ad40409ebf7f2fc4e1a703c57d3a90
Jacob Wiltse <aquabug@gmail.com>
2019-02-24 21:38:18 -0500

    some extra structs, start of websocket logic

--- structs.go
+++ structs.go
@@ -1,65 +1,64 @@
 package main
 
-
 type GatewayPayload struct {
-	Op uint32			`json:"op"`
-	Data interface{}	`json:"d"`
-	Sequence uint64		`json:"s"`
-	Event string		`json:"t"`
+	Op       uint32      `json:"op"`
+	Data     interface{} `json:"d"`
+	Sequence uint64      `json:"s"`
+	Event    string      `json:"t"`
 }
 
 type OutgoingPayload struct {
-	Op uint32 `json:"op"`
+	Op   uint32      `json:"op"`
 	Data interface{} `json:"d"`
 }
 
 type Identify struct {
-	Token string `json:"token"`
+	Token      string `json:"token"`
 	Properties struct {
-		OS string `json:"$os"`
+		OS      string `json:"$os"`
 		Browser string `json:"$browser"`
-		Device string `json:"$device"`
+		Device  string `json:"$device"`
 	} `json:"properties"`
-	Compress bool `json:"compress"`
-	LargeThreshold uint32 `json:"large_threshold"`
-	Shard []uint32 `json:"shard"`
+	Compress       bool     `json:"compress"`
+	LargeThreshold uint32   `json:"large_threshold"`
+	Shard          []uint32 `json:"shard"`
 }
 
 type Resume struct {
-	Token string `json:"token"`
+	Token     string `json:"token"`
 	SessionId string `json:"session_id"`
-	Sequence uint32 `json:"seq"`
+	Sequence  uint32 `json:"seq"`
 }
 
 type RequestGuildMembers struct {
 	GuildID string `json:"guild_id"`
-	Query string `json:"query"`
-	Limit uint32 `json:"limit"`
+	Query   string `json:"query"`
+	Limit   uint32 `json:"limit"`
 }
 
 type PresenceUpdatePayload struct {
 	Since uint32 `json:"since"`
-	Game struct {
+	Game  struct {
 		Name string `json:"name"`
 		Type uint32 `json:"type"`
 	} `json:"game"`
 	Status string `json:"status"`
-	Afk bool `json:"afk"`
+	Afk    bool   `json:"afk"`
 }
 
 type Hello struct {
-	Interval uint32 `json:"heartbeat_interval"`
-	Trace []string `json:"_trace"`
+	Interval uint32   `json:"heartbeat_interval"`
+	Trace    []string `json:"_trace"`
 }
 
 type Ready struct {
-	Version uint32 `json:"v"`
-	User User `json:"user"`
+	Version         uint32   `json:"v"`
+	User            User     `json:"user"`
 	PrivateChannels []string `json:"private_channels"`
-	Guilds []Guild `json:"guilds"`
-	SessionId string `json:"session_id"`
-	Trace []string `json:"_trace"`
-	Shard []uint32 `json:"shard"`
+	Guilds          []Guild  `json:"guilds"`
+	SessionId       string   `json:"session_id"`
+	Trace           []string `json:"_trace"`
+	Shard           []uint32 `json:"shard"`
 }
 
 type Resumed struct {
@@ -69,101 +68,122 @@ type Resumed struct {
 /* Gateway objects */
 
 type User struct {
-	Id string `json:"id"`
-	Username string `json:"username"`
+	Id            string `json:"id"`
+	Username      string `json:"username"`
 	Discriminator string `json:"discriminator"`
-	Avatar string `json:"avatar"`
-	Bot bool `json:"bot"`
-	MfaEnabled bool `json:"mfa_enabled"`
-	Locale string `json:"locale"`
-	Verified bool `json:"verified"`
-	Email string `json:"email"`
-	Flags uint32 `json:"flags"`
-	Premium uint32 `json:"premium_type"`
+	Avatar        string `json:"avatar"`
+	Bot           bool   `json:"bot"`
+	MfaEnabled    bool   `json:"mfa_enabled"`
+	Locale        string `json:"locale"`
+	Verified      bool   `json:"verified"`
+	Email         string `json:"email"`
+	Flags         uint32 `json:"flags"`
+	Premium       uint32 `json:"premium_type"`
 }
 
 type Guild struct {
-	Id string `json:"id"`
-	Name string `json:"name"`
-	Icon string `json:"icon"`
-	Splash string `json:"splash"`
-	Owner bool `json:"owner"`
-	OwnerId string `json:"owner_id"`
-	Permissions uint32 `json:"permissions"`
-	Region string `json:"region"`
-	AfkChannelId string `json:"afk_channel_id"`
-	AfkTimeout uint32 `json:"afk_timeout"`
-	EmbedEnabled bool `json:"embed_enabled"`
-	EmbedChannelId string `json:"embed_channel_id"`
-	VerificationLevel uint32 `json:"verification_level"`
-	DefaultMessageNotifications uint32 `json:"default_message_notifications"`
-	ExplicitContentFilter uint32 `json:"explicit_content_filter"`
-	Roles []Role `json:"roles"`
-	Emojis []Emoji `json:"emojis"`
-	Features []string `json:"features"`
-	MfaLevel uint32 `json:"mfa_level"`
-	ApplicationId string `json:"application_id"`
-	WidgetEnabled bool `json:"widget_enabled"`
-	WidgetChannelId string `json:"widget_channel_id"`
-	SystemChannelId string `json:"system_channel_id"`
-	JoinedAt string `json:"joined_at"`
-	Large bool `json:"large"`
-	Unavailable bool `json:"unavailable"`
-	MemberCount uint32 `json:"member_count"`
-	VoiceStates []VoiceState `json:"voice_states"`
-	Members []GuildMember `json:"members"`
-	Channels []Channel `json:"channels"`
-	Presences []PresenceUpdate `json:"presences"`
+	Id                          string           `json:"id"`
+	Name                        string           `json:"name"`
+	Icon                        string           `json:"icon"`
+	Splash                      string           `json:"splash"`
+	Owner                       bool             `json:"owner"`
+	OwnerId                     string           `json:"owner_id"`
+	Permissions                 uint32           `json:"permissions"`
+	Region                      string           `json:"region"`
+	AfkChannelId                string           `json:"afk_channel_id"`
+	AfkTimeout                  uint32           `json:"afk_timeout"`
+	EmbedEnabled                bool             `json:"embed_enabled"`
+	EmbedChannelId              string           `json:"embed_channel_id"`
+	VerificationLevel           uint32           `json:"verification_level"`
+	DefaultMessageNotifications uint32           `json:"default_message_notifications"`
+	ExplicitContentFilter       uint32           `json:"explicit_content_filter"`
+	Roles                       []Role           `json:"roles"`
+	Emojis                      []Emoji          `json:"emojis"`
+	Features                    []string         `json:"features"`
+	MfaLevel                    uint32           `json:"mfa_level"`
+	ApplicationId               string           `json:"application_id"`
+	WidgetEnabled               bool             `json:"widget_enabled"`
+	WidgetChannelId             string           `json:"widget_channel_id"`
+	SystemChannelId             string           `json:"system_channel_id"`
+	JoinedAt                    string           `json:"joined_at"`
+	Large                       bool             `json:"large"`
+	Unavailable                 bool             `json:"unavailable"`
+	MemberCount                 uint32           `json:"member_count"`
+	VoiceStates                 []VoiceState     `json:"voice_states"`
+	Members                     []GuildMember    `json:"members"`
+	Channels                    []Channel        `json:"channels"`
+	Presences                   []PresenceUpdate `json:"presences"`
 }
 
 type Role struct {
-	Id string `json:"id"`
-	Name string `json:"name"`
-	Color uint32 `json:"color"`
-	Hoist bool `json:"hoist"`
-	Position uint32 `json:"position"`
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Color       uint32 `json:"color"`
+	Hoist       bool   `json:"hoist"`
+	Position    uint32 `json:"position"`
 	Permissions uint32 `json:"permissions"`
-	Managed bool `json:"managed"`
-	Mentionable bool `json:"mentionable"`
+	Managed     bool   `json:"managed"`
+	Mentionable bool   `json:"mentionable"`
 }
 
 type Emoji struct {
-	Id string `json:"id"`
-	Name string `json:"name"`
-	Roles []string `json:"roles"`
-	User User `json:"user"`
-	RequireColons bool `json:"require_colons"`
-	Managed bool `json:"managed"`
-	Animated bool `json:"animated"`
+	Id            string   `json:"id"`
+	Name          string   `json:"name"`
+	Roles         []string `json:"roles"`
+	User          User     `json:"user"`
+	RequireColons bool     `json:"require_colons"`
+	Managed       bool     `json:"managed"`
+	Animated      bool     `json:"animated"`
 }
 
 type VoiceState struct {
-	GuildId string `json:"guild_id"`
-	ChannelId string `json:"channel_id"`
-	UserId string `json:"user_id"`
-	Member GuildMember `json:"member"`
-	SessionId string `json:"session_id"`
-	Deafened bool `json:"deaf"`
-	Muted bool `json:"mute"`
-	SelfDeafened bool `json:"self_deaf"`
-	SelfMuted bool `json:"self_mute"`
-	Suppressed bool `json:"suppress"`
+	GuildId      string      `json:"guild_id"`
+	ChannelId    string      `json:"channel_id"`
+	UserId       string      `json:"user_id"`
+	Member       GuildMember `json:"member"`
+	SessionId    string      `json:"session_id"`
+	Deafened     bool        `json:"deaf"`
+	Muted        bool        `json:"mute"`
+	SelfDeafened bool        `json:"self_deaf"`
+	SelfMuted    bool        `json:"self_mute"`
+	Suppressed   bool        `json:"suppress"`
 }
 
 type GuildMember struct {
-	User User `json:"user"`
-	Nick string `json:"nick"`
-	Roles []string `json:"roles"`
-	JoinedAt string `json:"joined_at"`
-	Deafened bool `json:"deaf"`
-	Muted bool `json:"mute"`
+	User     User     `json:"user"`
+	Nick     string   `json:"nick"`
+	Roles    []string `json:"roles"`
+	JoinedAt string   `json:"joined_at"`
+	Deafened bool     `json:"deaf"`
+	Muted    bool     `json:"mute"`
 }
 
 type Channel struct {
-	Id string `json:"id"`
-	Type uint32 `json:"type"`
-	GuildId string `json:"guild_id"`
-
+	Id               string      `json:"id"`
+	Type             uint32      `json:"type"`
+	GuildId          string      `json:"guild_id"`
+	Position         uint32      `json:"position"`
+	Overwrites       []Overwrite `json:"permission_overwrites"`
+	Name             string      `json:"name"`
+	Topic            string      `json:"topic"`
+	NSFW             bool        `json:"nsfw"`
+	LastMessageId    string      `json:"last_message_id"`
+	Bitrate          uint32      `json:"bitrate"`
+	UserLimit        uint32      `json:"user_limit"`
+	Slowmode         uint32      `json:"rate_limit_per_user"`
+	Recipients       []User      `json:"recipients"`
+	Icon             string      `json:"icon"`
+	OwnerId          string      `json:"owner_id"`
+	ApplicationId    string      `json:"application_id"`
+	ParentId         string      `json:"parent_id"`
+	LastPinTimestamp string      `json:"last_pin_timestamp"`
 }
 
-type PresenceUpdate struct {}
+type Overwrite struct {
+	Id    string `json:"id"`
+	Type  string `json:"type"`
+	Allow uint32 `json:"allow"`
+	Deny  uint32 `json:"deny"`
+}
+
+type PresenceUpdate struct{}
--- ws.go
+++ ws.go
@@ -1,16 +1,28 @@
 package main
 
-import "github.com/gorilla/websocket"
+import (
+	"github.com/go-redis/redis"
+	"github.com/gorilla/websocket"
+	"github.com/nats-io/go-nats"
+	"github.com/nats-io/go-nats-streaming"
+)
 
 type Shard struct {
-	Token string
-	SessionID string
-	Sequence int64
-	Conn *websocket.Conn
-	ShardId uint16
+	Token      string
+	SessionID  string
+	Sequence   int64
+	Conn       *websocket.Conn
+	ShardId    uint16
 	ShardCount uint16
+	Cache      *redis.Client
+	NC         *nats.Conn
+	SC         *stan.Conn
 }
 
-func (s *Shard) onMessage(messageType int, message []byte) (*Event, error) {
+func (s *Shard) onPayload(messageType int, message []byte) (*Event, error) {
 
+}
+
+func (s *Shard) run() {
+	s.Cache = redis.NewClient()
 }
